bc9b34eb1feca7979b1437b8e7453da4c1de3990
Bilal Abbas <bilal.abbas@gigalabs.co>
2021-08-04 14:06:38 +0500

    complete source code

---
+++ lib/robot.rb
@@ -0,0 +1,52 @@
+class Robot
+  attr_reader :table, :compass
+
+  def initialize(table, compass = Compass.new)
+    @table = table
+    @compass = compass
+  end
+
+  def place(position, direction)
+    direction = direction.to_s.upcase
+    unless valid_place?(position, direction)
+      puts "Out-of-bounds. Please use dimensions 5 units x 5 units."
+      return 
+    end
+
+    @position = position
+    compass.direction = direction
+  end
+
+  def move
+    return unless has_position?
+
+    new_position = @position.move_to(compass.direction)
+    @position = new_position if valid_position?(new_position)
+  end
+
+  def left
+    compass.left if has_position?
+  end
+
+  def right
+    compass.right if has_position?
+  end
+
+  def report
+    "#{@position},#{compass.direction}" if has_position?
+  end
+
+  private
+
+  def has_position?
+    !!@position
+  end
+
+  def valid_place?(position, direction)
+    valid_position?(position) && compass.valid_direction?(direction)
+  end
+
+  def valid_position?(position)
+    table.within_bounds?(position)
+  end
+end
